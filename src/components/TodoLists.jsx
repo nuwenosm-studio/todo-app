29e0ad68034e228d4ffdd0ff6be9fa3ebb9deb85
nuwenosm <nuwenosm@gmail.com>
2023-06-04 15:37:19 -0700

    add todo_lists with style only

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { BsCheckLg } from "react-icons/bs";
+import { TbEdit } from "react-icons/tb";
 
 const TodoLists = () => {
   return (
-    <div>TodoLists</div>
+    <div className="todo_lists">
+      <div className="todo_item">
+        <div className="todo_task">I watch movie</div>
+        <div className="finish_button">
+          <BsCheckLg fontSize="20px"/>
+        </div>
+      </div>
+      <div className="todo_item">
+        <div className="todo_task">I watch movie</div>
+        <div className="finish_button">
+          <BsCheckLg fontSize="20px"/>
+        </div>
+      </div>
+      <div className="todo_item">
+        <div className="todo_task">I watch movie</div>
+        <div className="finish_button">
+          <BsCheckLg fontSize="20px"/>
+        </div>
+      </div>
+
+      <div className="edit_button">
+        Edit <TbEdit fontSize="20px"/>
+      </div>
+    </div>
   )
 }
 
